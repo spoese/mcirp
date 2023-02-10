912d1ea8341f8d45665b8735ea86e8a7c68493e7
Scott Poese <39490812+spoese@users.noreply.github.com>
2023-02-10 10:26:14 -0500

    Add columns to retention.
    Ensures the retention function keeps all original data from the input data frame.

--- R/retention.R
+++ R/retention.R
@@ -1,10 +1,10 @@
 #' Determining retention status for each student
-#' 
+#'
 #' `retention()` takes a data frame that includes at least an identification
 #' number for each student and a term code for each student, and determines
 #' whether a student who was enrolled in a certain semester was also enrolled in
 #' another semester of the user's choice.
-#' 
+#'
 #' @param data Input data frame. This should have at least a column that uniquely
 #' identifies the students and a column that indicates the term code that the
 #' student was registered for.
@@ -18,13 +18,13 @@
 #' @returns A data frame of all students enrolled in `first_semester` with a
 #' new column of logicals added indicating whether the student was retained to
 #' `second_semester`.
-#' @examples 
+#' @examples
 #' df <- tibble(pidm = c(1:5, 1, 2, 5, 1, 4), term_code = c(rep(202120,5), rep(202220,3), rep(202320,2)))
 #' retention(df, 202120, 202220)
 #' @export
 retention <- function(data, first_semester, next_semester, pidm_col = 1, term_code_col = 2) {
         first_students <- data[as.vector(data[,term_code_col] == first_semester),]
         second_students <- data[as.vector(data[,term_code_col] == next_semester),]
-        first_students[,pidm_col] |> 
+        first_students |>
                 mutate(Retained = pull(first_students,pidm_col) %in% pull(second_students,pidm_col))
 }
